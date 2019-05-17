b6289006e91eac8903164fb4addc4cd35f2926b9
Stuart Work <91031442@mail.computerpower.ac.nz>
2019-05-18 09:43:34 +1200

    Array commit 3

--- Documents/NetBeansProjects/ArrayTasks/src/arraytasks/ArrayTasks.java
+++ Documents/NetBeansProjects/ArrayTasks/src/arraytasks/ArrayTasks.java
@@ -12,9 +12,10 @@ import java.util.Arrays;
 public class ArrayTasks {
     
     private static int gradeArraySize = 5;
+    private static int subjectArraySize = 3;
     
     public static char[] studentGrade = new char[gradeArraySize];
-
+    public static String [] subjectNames = new String[gradeArraySize];
     /**
      * @param args the command line arguments
      */
@@ -29,6 +30,7 @@ public class ArrayTasks {
         displayCharArray(studentGrade);
         
         char [] newStudentGrade = new char[gradeArraySize];
+        String[] newSubjectArray;
         
         System.arraycopy(studentGrade, 0, newStudentGrade, 0, 2);
         newStudentGrade[2] = 'C';
@@ -37,7 +39,27 @@ public class ArrayTasks {
         System.out.println("Correct grade array:");
         displayCharArray(newStudentGrade);        
 
+        subjectNames[0] = "Mathematics";
+        subjectNames[1] = "Science";
+        subjectNames[2] = "Physical Education";
         
-    }
-    
+        System.out.println("Second element in subjects array:" + subjectNames[1] + "\n");
+         
+        newSubjectArray = Arrays.copyOf(subjectNames, 5);
+        newSubjectArray[3] = "English";
+        newSubjectArray[4] = "Fine Arts";
+        
+        System.out.println("All subjects array:");
+        for (int i = 0; i< newSubjectArray.length; i++){
+            System.out.println(newSubjectArray[i]);
+        }
+    }       
+ public static void displayCharArray(char[] arrayToDisplay){
+        for (int i = 0; i< arrayToDisplay.length; i++){
+            System.out.println(arrayToDisplay[i]);
+        }
+        System.out.println("\n");
+        }
+        
+        
 }
