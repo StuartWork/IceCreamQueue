7487803cd37e480a1190269ec1b32339938ccf24
Stuart Work <91031442@mail.computerpower.ac.nz>
2019-05-18 15:06:18 +1200

    Array List Shopping/Person commit 8

--- Documents/NetBeansProjects/ArrayListTasks/src/arraylisttasks/Person.java
+++ Documents/NetBeansProjects/ArrayListTasks/src/arraylisttasks/Person.java
@@ -23,10 +23,34 @@ public class Person {
     
     }
     
+    public String getId(){
+        return id;
+    }
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getWeight(){
+        return weight;
+    }
+    
+    public void setWeight(String weiht){
+        this.weight = weight;
+    }
+    
     @Override
     public String toString(){
         
-        return "Person Information:\nID:" + id + "\nName:" + name + "\nWeight:" + weight;
+        return "Person Information:\nID: " + id + "\nName: " + name + "\nWeight: " + weight;
     
     }
     
