1c7cf1e9d45c5ef2e4c53ad6bbfef887f0d07f35
Stuart Work <91031442@mail.computerpower.ac.nz>
2019-05-18 14:27:58 +1200

    Array List Shopping commit 1

---
+++ Documents/NetBeansProjects/ArrayListTasks/src/arraylisttasks/ArrayListTasks.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraylisttasks;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Stuart
+ */
+public class ArrayListTasks {
+    
+    public static ArrayList shoppingList = new ArrayList();
+    
+    public static void main(String[] args){
+        
+        shoppingList.add();
+        shoppingList.add();
+        shoppingList.add();
+        shoppingList.add();
+    
+    }
+    
+    private static void printShoppingList(ArrayList list){
+        
+        System.out.println("Shopping list:");
+        for (Object s: list){
+            System.out.println(s);        
+        }
+        System.out.println("  ");
+    
+    }
+    
+}
