2c907d1321a064f0532c9b9669c55951f3c37d0d
Stuart Work <91031442@mail.computerpower.ac.nz>
2019-05-19 13:45:03 +1200

    Collection Task version 1

---
+++ Documents/NetBeansProjects/CollectionInterfaceTask/src/collectioninterfacetask/CollectionInterfaceTask.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collectioninterfacetask;
+
+/**
+ *
+ * @author Stuart
+ */
+public class CollectionInterfaceTask {
+    
+    private static ArrayList myArrayList = new ArrayList();
+    private static LinkedList myLinkedList = new LinkedList();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
